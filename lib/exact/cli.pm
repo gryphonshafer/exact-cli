169fa91f31b8fe68284bc46bc25ea2988e068e89
Gryphon Shafer <gryphon@goldenguru.com>
2020-03-05 13:53:16 -0800

    Documentation and TravisCI 2020 OCD

--- lib/exact/cli.pm
+++ lib/exact/cli.pm
@@ -102,9 +102,7 @@ You can look for additional information at:
 
 =for :list
 * L<GitHub|https://github.com/gryphonshafer/exact-cli>
-* L<CPAN|http://search.cpan.org/dist/exact-cli>
 * L<MetaCPAN|https://metacpan.org/pod/exact::cli>
-* L<AnnoCPAN|http://annocpan.org/dist/exact-cli>
 * L<Travis CI|https://travis-ci.org/gryphonshafer/exact-cli>
 * L<Coveralls|https://coveralls.io/r/gryphonshafer/exact-cli>
 * L<CPANTS|http://cpants.cpanauthors.org/dist/exact-cli>
